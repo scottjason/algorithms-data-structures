5b4a69a5d1c02b7857042b3ac0b289941f919965
Scott Jason <scottleejason@gmail.com>
2020-11-01 15:31:40 -0800

    add maxchars

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,37 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+// Solution 1
+// function maxChar(str) {
+//   const map = {};
+//   str.split('').forEach(char => {
+//     map[char] = map[char] ? map[char] + 1 : 1;
+//   });
+
+//   let maxUsedChar = 0;
+//   Object.keys(map).forEach(key => {
+//     if (map[key] > maxUsedChar) {
+//       maxUsedChar = key;
+//     }
+//   });
+//   return maxUsedChar
+// }
+
+// Solution 2
+function maxChar(str) {
+  const map = {};
+  for (let char of str) {
+    map[char] = map[char] ? map[char] + 1 : 1;
+  }
+  let mostUsedChar;
+  let maxTimesUsed = 0;
+  Object.keys(map).forEach(key => {
+    if (map[key] > maxTimesUsed) {
+      maxTimesUsed = map[key];
+      mostUsedChar = key;
+    }
+  });
+  return mostUsedChar
+}
 
 module.exports = maxChar;
